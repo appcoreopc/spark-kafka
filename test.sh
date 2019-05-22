a0a9529fd34fb9aadbca7f6050184a5c31d1188b
jeremy woo <kepungmoin@hotmail.com>
2019-05-22 20:10:44 +1200

    added port info

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
-spark-submit --packages --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0  kafka.py localhost subscribe test
-spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:2.4.3 kafka.py localhost subscribe test,testtoc
 
+bin/kafka-console-producer.sh --broker-list localhost:9092 --topic test
 
 
 bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test
+
+
+spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.1 kafka.py localhost:9092 subscribe test11
